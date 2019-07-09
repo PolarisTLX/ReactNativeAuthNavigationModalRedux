6a6117ec63f69682a03c67e73437c8b09ef83913
Paul Rail <paul.a.rail@gmail.com>
2019-07-09 15:42:06 -0400

    catch loginUserFail situation

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,7 +1,9 @@
 import firebase from 'firebase';
 import { 
   EMAIL_CHANGED, 
-  PASSWORD_CHANGED
+  PASSWORD_CHANGED,
+  LOGIN_USER_SUCCESS,
+  LOGIN_USER_FAIL
 } from './types';
 
 export const emailChanged = (text) => {
@@ -18,12 +20,44 @@ export const passwordChanged = (text) => {
   };
 };
 
+// before "cleaning up this code" with the helper(s) below:
+// export const loginUser = ({ email, password }) => {
+//   return (dispatch) => {
+//     firebase.auth().signInWithEmailAndPassword(email, password)
+//       .then(user => {
+//         // the fact that we can have this 'dispatch' and manually call ONLY AFTER the '.then' is what makes redux thunk work in asynchronous fasion:
+//         dispatch({ type: LOGIN_USER_SUCCESS, payload: user });
+//       })
+//       .catch(() => {
+//         firebase.auth().createUserWithEmailAndPassword(email, password)
+//           .then(user => {
+//             dispatch({ type: LOGIN_USER_SUCCESS, payload: user });
+//           });
+//       });
+//   };
+// };
+
 export const loginUser = ({ email, password }) => {
   return (dispatch) => {
     firebase.auth().signInWithEmailAndPassword(email, password)
-      .then(user => {
-        // the fact that we can have this 'dispatch' and manually call ONLY AFTER the '.then' is what makes redux thunk work in asynchronous fasion:
-        dispatch({ type: 'LOGIN_USER_SUCCESS', payload: user });
+    // the fact that we can have this 'dispatch' and manually call ONLY AFTER the '.then' is what makes redux thunk work in asynchronous fasion:
+      .then(user => loginUserSuccess(dispatch, user))
+      .catch(() => {
+        firebase.auth().createUserWithEmailAndPassword(email, password)
+          .then(user => loginUserSuccess(dispatch, user))
+          .catch(() => loginUserFail(dispatch));
       });
   };
 };
+
+const loginUserSuccess = (dispatch, user) => {
+  // the fact that we can have this 'dispatch' and manually call ONLY AFTER the '.then' is what makes redux thunk work in asynchronous fasion:
+  dispatch({
+    type: LOGIN_USER_SUCCESS,
+    payload: user
+  });
+};
+
+const loginUserFail = (dispatch) => {
+  dispatch({ type: LOGIN_USER_FAIL });
+}
