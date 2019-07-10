dcf60681ed1025534d86966ad5ccf4c4419958bd
Paul Rail <paul.a.rail@gmail.com>
2019-07-10 18:39:04 -0400

    Create Employee form is all done with a day-of-the-week picker

--- src/components/EmployeeCreate.js
+++ src/components/EmployeeCreate.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import { Picker, Text } from 'react-native';
 import { employeeUpdate } from '../actions';
 import { Card, CardSection, Input, Button } from './common';
 
@@ -26,12 +27,36 @@ class EmployeeCreate extends Component {
         </CardSection>
 
         <CardSection>
+          <Text styles={styles.pickerLabelStyle}>Shift</Text>
+          <Picker
+            style={{ flex: 1, marginTop: 10 }}
+            selectedValue={this.props.shift}
+            onValueChange={day => this.props.employeeUpdate({ prop: 'shift', value: day })}
+          >
+            <Picker.Item label="Monday" value="Monday" />
+            <Picker.Item label="Tuesday" value="Tuesday" />
+            <Picker.Item label="Wednesday" value="Wednesday" />
+            <Picker.Item label="Thursday" value="Thursday" />
+            <Picker.Item label="Friday" value="Friday" />
+            <Picker.Item label="Saturday" value="Saturday" />
+            <Picker.Item label="Sunday" value="Sunday" />
+          </Picker>
+        </CardSection>
+
+        <CardSection>
           <Button>
             Create
           </Button>
         </CardSection>
       </Card>
      );
+  }
+}
+
+const styles = {
+  pickerLabelStyle: {
+    fontSize: 18,  // size not working?
+    paddingLeft: 20
   }
 }
 
--- src/components/common/CardSection.js
+++ src/components/common/CardSection.js
@@ -3,7 +3,9 @@ import { View } from 'react-native';
 
 const CardSection = (props) => {
   return (
-    <View style={styles.containerStyle}>
+    // <View style={styles.containerStyle}>
+    // below allows for you to optionally pass in different styles that overide the ones coded here in this file:
+    <View style={[styles.containerStyle, props.style]}>
       {props.children}
     </View>
   );
