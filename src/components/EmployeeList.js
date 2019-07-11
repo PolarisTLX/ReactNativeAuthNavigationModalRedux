7e065d426beabf4accdfa2e4b9a05b4d71de7265
Paul Rail <paul.a.rail@gmail.com>
2019-07-10 20:38:28 -0400

    using a DEPRECATED ListView element

--- src/components/EmployeeList.js
+++ src/components/EmployeeList.js
@@ -1,17 +1,49 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
-import { View, Text } from 'react-native';
+import { View, Text, ListView } from 'react-native';
 import { employeesFetch } from '../actions';
 
 
 class EmployeeList extends Component {
 
+  // THIS IS DEPRECATED!!! ANYTHING WITH ListView IS DEPRECATED!
   componentWillMount() {
     this.props.employeesFetch();
+
+    this.createDataSource(this.props);
+  }
+
+  componentWillReceiveProps(nextProps) {
+    // nextProps are the next set of props that this component
+    // will be rendered with
+    // this.props is still the old set of props
+
+    this.createDataSource(nextProps);
+  }
+
+  createDataSource({ employees }) {
+    // ds = data-source
+    const ds = new ListView.DataSource({
+      rowHasChanged: (r1, r2) => r1 !== r2
+    });
+
+    this.dataSource = ds.cloneWithRows(employees);
+  }
+
+  renderRow(employee) {
+    return <ListItem employee={employee} />;
   }
 
   render() {
     return (
+      // DEPRECATED!!!
+      <ListView
+        enableEmptySections
+        dataSource={this.dataSource}
+        renderRow={this.renderRow}
+      />
+
+      // hard coded examples:
       <View>
         <Text>Employee List</Text>
         <Text>Employee List</Text>
@@ -19,7 +51,7 @@ class EmployeeList extends Component {
         <Text>Employee List</Text>
         <Text>Employee List</Text>
       </View>
-     );
+    );
   }
 }
  
