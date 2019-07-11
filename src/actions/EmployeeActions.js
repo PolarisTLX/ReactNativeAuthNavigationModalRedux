62f2afea45b59a7e789801f004182e2365f88e6d
Paul Rail <paul.a.rail@gmail.com>
2019-07-10 20:03:05 -0400

    issues with Firebase online console 'rules'

--- src/actions/EmployeeActions.js
+++ src/actions/EmployeeActions.js
@@ -1,10 +1,41 @@
+import firebase from 'firebase';
+import { Actions } from 'react-native-router-flux';
+
 import {
-  EMPLOYEE_UPDATE
+  EMPLOYEE_UPDATE,
+  EMPLOYEE_CREATE
 } from './types';
 
 export const employeeUpdate = ({ prop, value }) => {
   return {
     type: EMPLOYEE_UPDATE,
     payload: { prop, value }
+  };
+};
+
+export const employeeCreate = ({ name, phone, shift }) => {
+  // the current user:
+  const { currentUser } = firebase.auth();
+
+  return (dispatch) => {  //this line/wrapper needed to avoid an error using redux-thunk
+    
+    // need to update the firebase rules for their Version 2 that is different from the course.
+    /*
+    rules_version = '2';
+    service cloud.firestore {
+      match / users / { user } {
+        allow read, write;
+        match / employees / { employee } {
+          allow read, write;
+        }
+      }
+    }
+    */
+    firebase.database().ref(`/users/${currentUser.uid}/employees`)
+      .push({ name, phone, shift })
+      .then(() => {
+        dispatch({ type: EMPLOYEE_CREATE });
+        Actions.employeeList({ type: 'reset' });
+      });
   };
 };
--- src/reducers/EmployeeFormReducer.js
+++ src/reducers/EmployeeFormReducer.js
@@ -1,5 +1,6 @@
 import {
-  EMPLOYEE_UPDATE
+  EMPLOYEE_UPDATE,
+  EMPLOYEE_CREATE
 } from '../actions/types';
 
 const INITIAL_STATE = {
@@ -21,6 +22,9 @@ export default (state = INITIAL_STATE, action) => {
       // square braces [] below is not an array, it is key-interpolation
       // '[action.payload.prop]' below gets converted into whatever gets passed
       return { ...state, [action.payload.prop]: action.payload.value };
+    case EMPLOYEE_CREATE:
+      // reset form to be blank after an employee has been created/submitted:
+      return INITIAL_STATE;
     default:
       return state;
   }
